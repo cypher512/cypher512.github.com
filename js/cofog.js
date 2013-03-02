fe6215e0eea725bc5c0eb08476e49c1ac691709e
anonymous <an.address.in.the.net@gmail.com>
2013-03-02 22:31:08 +0900

    test

--- js/cofog.js
+++ js/cofog.js
@@ -10,10 +10,10 @@ OpenSpending.Styles.Cofog = {
 
     /* sendai cofog */
 
-	'01'  : { icon: 'icons/government-uk.svg',  color: '#00CC00' },
+	'01': { icon: 'icons/government-uk.svg',  color: '#00CC00' },
 	'01.1': { icon: 'icons/government-uk.svg',  color: '#00CC00' },
 
-	'02'  : { icon: 'icons/government.svg', color: '#00CC00' },
+	'02': { icon: 'icons/government.svg', color: '#00CC00' },
 	'02.1': { icon: 'icons/government.svg', color: '#00CC00' },
 	'02.2': { icon: 'icons/government.svg', color: '#00CC00' },
 	'02.3': { icon: 'icons/government.svg', color: '#00CC00' },
@@ -21,12 +21,12 @@ OpenSpending.Styles.Cofog = {
 	'02.5': { icon: 'icons/government.svg', color: '#00CC00' },
 	'02.6': { icon: 'icons/government.svg', color: '#00CC00' },
 
-	'03'  : { icon: 'icons/civilian-action.svg', color: '#00CC00' },
+	'03': { icon: 'icons/civilian-action.svg', color: '#00CC00' },
 	'03.1': { icon: 'icons/civilian-action.svg', color: '#00CC00' },
 	'03.2': { icon: 'icons/civilian-action.svg', color: '#00CC00' },
 	'03.3': { icon: 'icons/civilian-action.svg', color: '#00CC00' },
 
-	'04'  : { icon: 'icons/health.svg', color: '#00CC00' },
+	'04': { icon: 'icons/health.svg', color: '#00CC00' },
 	'04.1': { icon: 'icons/health.svg', color: '#00CC00' },
 	'04.2': { icon: 'icons/health.svg', color: '#00CC00' },
 	'04.3': { icon: 'icons/health.svg', color: '#00CC00' },
@@ -35,14 +35,14 @@ OpenSpending.Styles.Cofog = {
 	'04.6': { icon: 'icons/health.svg', color: '#00CC00' },
 	'04.7': { icon: 'icons/health.svg', color: '#00CC00' },
 
-	'05'  : { icon: 'icons/environment.svg', color: '#00CC00' },
+	'05': { icon: 'icons/environment.svg', color: '#00CC00' },
 	'05.1': { icon: 'icons/environment.svg', color: '#00CC00' },
 
-	'06'  : { icon: 'icons/economy-tourism.svg', color: '#00CC00' },
+	'06': { icon: 'icons/economy-tourism.svg', color: '#00CC00' },
 	'06.1': { icon: 'icons/economy-tourism.svg', color: '#00CC00' },
 	'06.2': { icon: 'icons/farms.svg',           color: '#00CC00' },
 
-	'07'  : { icon: 'icons/our-streets.svg', color: '#00CC00' },
+	'07': { icon: 'icons/our-streets.svg', color: '#00CC00' },
 	'07.1': { icon: 'icons/our-streets.svg', color: '#00CC00' },
 	'07.2': { icon: 'icons/our-streets.svg', color: '#00CC00' },
 	'07.3': { icon: 'icons/our-streets.svg', color: '#00CC00' },
@@ -51,10 +51,10 @@ OpenSpending.Styles.Cofog = {
 	'07.6': { icon: 'icons/our-streets.svg', color: '#00CC00' },
 	'07.7': { icon: 'icons/our-streets.svg', color: '#00CC00' },
 
-	'08'  : { icon: 'icons/fire-brigade.svg', color: '#00CC00' },
+	'08': { icon: 'icons/fire-brigade.svg', color: '#00CC00' },
 	'08.1': { icon: 'icons/fire-brigade.svg', color: '#00CC00' },
 
-	'09'  : { icon: 'icons/education.svg', color: '#00CC00' },
+	'09': { icon: 'icons/education.svg', color: '#00CC00' },
 	'09.1': { icon: 'icons/education.svg', color: '#00CC00' },
 	'09.2': { icon: 'icons/education.svg', color: '#00CC00' },
 	'09.3': { icon: 'icons/education.svg', color: '#00CC00' },
@@ -65,15 +65,15 @@ OpenSpending.Styles.Cofog = {
 	'09.8': { icon: 'icons/education.svg', color: '#00CC00' },
 	'09.9': { icon: 'icons/education.svg', color: '#00CC00' },
 
-	'10'  : { icon: 'icons/economic-aid.svg', color: '#00CC00' },
+	'10': { icon: 'icons/economic-aid.svg', color: '#00CC00' },
 	'10.1': { icon: 'icons/economic-aid.svg', color: '#00CC00' },
 
-	'11'  : { icon: 'icons/aid-developing-countries.svg', color: '#00CC00' },
+	'11': { icon: 'icons/aid-developing-countries.svg', color: '#00CC00' },
 	'11.1': { icon: 'icons/aid-developing-countries.svg', color: '#00CC00' },
 
-	'12'  : { icon: 'icons/publicaffairs.svg', color: '#00CC00' },
+	'12': { icon: 'icons/publicaffairs.svg', color: '#00CC00' },
 	'12.1': { icon: 'icons/publicaffairs.svg', color: '#00CC00' },
 
-	'13'  : { icon: 'icons/unknown.svg', color: '#00CC00' },
+	'13': { icon: 'icons/unknown.svg', color: '#00CC00' },
 	'13.1': { icon: 'icons/unknown.svg', color: '#00CC00' }
 };
