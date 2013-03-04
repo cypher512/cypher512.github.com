48d500a994d23faa9f86f5f27e96ef46e88fbf1e
anonymous <an.address.in.the.net@gmail.com>
2013-03-04 22:52:28 +0900

    test

--- js/bubbletree.cofog.js
+++ js/bubbletree.cofog.js
@@ -7,37 +7,76 @@ var BubbleTree = BubbleTree || {};
 BubbleTree.Styles = BubbleTree.Styles || {};
 
 BubbleTree.Styles.Cofog1 = {
-	'01': { icon: 'icons/government-uk.svg', color: '#00CC00' },
-	'02': { icon: 'icons/schools.svg', color: '#0AB971' },
-	'03': { icon: 'icons/civilian-action.svg', color: '#EC2406' },
-	'04': { icon: 'icons/order-safety.svg', color: '#790586' },
-	'05': { icon: 'icons/environment.svg', color: '#2A3A03' },
-	'06': { icon: 'icons/garbage.svg', color: '#D33673' },
-	'07': { icon: 'icons/our-streets.svg', color: '#D33673' },
-	'08': { icon: 'icons/economy-tourism.svg', color: '#4E6D00' },
-	'09': { icon: 'icons/government.svg', color: '#C75746' },
-	'10': { icon: 'icons/traffic-watersup.svg', color: '#938626' }
+        '01': { icon: 'icons/government-uk.svg',  color: '#00CC00' },
+	'02': { icon: 'icons/government.svg', color: '#00CC00' },
+	'03': { icon: 'icons/civilian-action.svg', color: '#00CC00' },
+	'04': { icon: 'icons/health.svg', color: '#00CC00' },
+	'05': { icon: 'icons/environment.svg', color: '#00CC00' },
+	'06': { icon: 'icons/economy-tourism.svg', color: '#00CC00' },
+	'07': { icon: 'icons/our-streets.svg', color: '#00CC00' },
+	'08': { icon: 'icons/fire-brigade.svg', color: '#00CC00' },
+	'09': { icon: 'icons/education.svg', color: '#00CC00' },
+	'10': { icon: 'icons/economic-aid.svg', color: '#00CC00' },
+	'11': { icon: 'icons/aid-developing-countries.svg', color: '#00CC00' },
+	'12': { icon: 'icons/publicaffairs.svg', color: '#00CC00' },
+	'13': { icon: 'icons/money.svg', color: '#00CC00' }
 }
 
 BubbleTree.Styles.Cofog2 = {
     /* sendai cofog */
-	'01.1': { icon: 'icons/government-uk.svg'},
-	'02.1': { icon: 'icons/pre-school.svg'},
-	'02.2': { icon: 'icons/education.svg'},
-	'03.1': { icon: 'icons/civilian-action.svg'},
-	'04.1': { icon: 'icons/order-safety.svg'},
-	'05.1': { icon: 'icons/wind.svg'},
-	'05.2': { icon: 'icons/tree.svg'},
-	'06.1': { icon: 'icons/garbage.svg'},
-	'07.1': { icon: 'icons/housing.svg'},
-	'07.2': { icon: 'icons/street-lights.svg'},
-	'07.3': { icon: 'icons/farms.svg'},
-	'08.1': { icon: 'icons/culture.svg'},
-	'08.2': { icon: 'icons/dollar.svg'},
-	'08.3': { icon: 'icons/harbor.svg'},
-	'09.1': { icon: 'icons/publicaffairs.svg'},
-	'09.2': { icon: 'icons/government.svg'},
-	'10.1': { icon: 'icons/traffic-watersup.svg'}
+	'01.1': { icon: 'icons/government-uk.svg',  color: '#00CC00' },
+
+	'02.1': { icon: 'icons/government.svg', color: '#00CC00' },
+	'02.2': { icon: 'icons/government.svg', color: '#00CC00' },
+	'02.3': { icon: 'icons/government.svg', color: '#00CC00' },
+	'02.4': { icon: 'icons/government.svg', color: '#00CC00' },
+	'02.5': { icon: 'icons/government.svg', color: '#00CC00' },
+	'02.6': { icon: 'icons/government.svg', color: '#00CC00' },
+
+	'03.1': { icon: 'icons/civilian-action.svg', color: '#00CC00' },
+	'03.2': { icon: 'icons/civilian-action.svg', color: '#00CC00' },
+	'03.3': { icon: 'icons/civilian-action.svg', color: '#00CC00' },
+
+	'04.1': { icon: 'icons/health.svg', color: '#00CC00' },
+	'04.2': { icon: 'icons/health.svg', color: '#00CC00' },
+	'04.3': { icon: 'icons/health.svg', color: '#00CC00' },
+	'04.4': { icon: 'icons/health.svg', color: '#00CC00' },
+	'04.5': { icon: 'icons/health.svg', color: '#00CC00' },
+	'04.6': { icon: 'icons/health.svg', color: '#00CC00' },
+	'04.7': { icon: 'icons/health.svg', color: '#00CC00' },
+
+	'05.1': { icon: 'icons/environment.svg', color: '#00CC00' },
+
+	'06.1': { icon: 'icons/economy-tourism.svg', color: '#00CC00' },
+	'06.2': { icon: 'icons/farms.svg',           color: '#00CC00' },
+
+	'07.1': { icon: 'icons/our-streets.svg', color: '#00CC00' },
+	'07.2': { icon: 'icons/our-streets.svg', color: '#00CC00' },
+	'07.3': { icon: 'icons/our-streets.svg', color: '#00CC00' },
+	'07.4': { icon: 'icons/our-streets.svg', color: '#00CC00' },
+	'07.5': { icon: 'icons/our-streets.svg', color: '#00CC00' },
+	'07.6': { icon: 'icons/our-streets.svg', color: '#00CC00' },
+	'07.7': { icon: 'icons/our-streets.svg', color: '#00CC00' },
+
+	'08.1': { icon: 'icons/fire-brigade.svg', color: '#00CC00' },
+
+	'09.1': { icon: 'icons/education.svg', color: '#00CC00' },
+	'09.2': { icon: 'icons/education.svg', color: '#00CC00' },
+	'09.3': { icon: 'icons/education.svg', color: '#00CC00' },
+	'09.4': { icon: 'icons/education.svg', color: '#00CC00' },
+	'09.5': { icon: 'icons/education.svg', color: '#00CC00' },
+	'09.6': { icon: 'icons/education.svg', color: '#00CC00' },
+	'09.7': { icon: 'icons/education.svg', color: '#00CC00' },
+	'09.8': { icon: 'icons/education.svg', color: '#00CC00' },
+	'09.9': { icon: 'icons/education.svg', color: '#00CC00' },
+
+	'10.1': { icon: 'icons/economic-aid.svg', color: '#00CC00' },
+
+	'11.1': { icon: 'icons/aid-developing-countries.svg', color: '#00CC00' },
+
+	'12.1': { icon: 'icons/publicaffairs.svg', color: '#00CC00' },
+
+	'13.1': { icon: 'icons/money.svg', color: '#00CC00' }
 };
 
 BubbleTree.Styles.Cofog3 = {
