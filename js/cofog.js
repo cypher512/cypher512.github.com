2c3a132590f2a49475bc2eab5bc530f74f8aaedc
cypher512 <saeki512000gmail.com>
2013-03-10 21:53:45 +0900

    南三陸用に変更

--- js/cofog.js
+++ js/cofog.js
@@ -8,7 +8,7 @@ OpenSpending.Styles = OpenSpending.Styles || {};
 
 OpenSpending.Styles.Cofog = {
 
-    /* sendai cofog */
+    /* minamisanriku cofog */
 
 	'01': { icon: 'icons/government-uk.svg',  color: '#00CC00' },
 	'01.1': { icon: 'icons/government-uk.svg',  color: '#00CC00' },
@@ -31,16 +31,14 @@ OpenSpending.Styles.Cofog = {
 	'04.2': { icon: 'icons/health.svg', color: '#00CC00' },
 	'04.3': { icon: 'icons/health.svg', color: '#00CC00' },
 	'04.4': { icon: 'icons/health.svg', color: '#00CC00' },
-	'04.5': { icon: 'icons/health.svg', color: '#00CC00' },
-	'04.6': { icon: 'icons/health.svg', color: '#00CC00' },
-	'04.7': { icon: 'icons/health.svg', color: '#00CC00' },
 
 	'05': { icon: 'icons/environment.svg', color: '#00CC00' },
 	'05.1': { icon: 'icons/environment.svg', color: '#00CC00' },
+	'05.2': { icon: 'icons/environment.svg', color: '#00CC00' },
+	'05.3': { icon: 'icons/environment.svg', color: '#00CC00' },
 
 	'06': { icon: 'icons/economy-tourism.svg', color: '#00CC00' },
 	'06.1': { icon: 'icons/economy-tourism.svg', color: '#00CC00' },
-	'06.2': { icon: 'icons/farms.svg',           color: '#00CC00' },
 
 	'07': { icon: 'icons/our-streets.svg', color: '#00CC00' },
 	'07.1': { icon: 'icons/our-streets.svg', color: '#00CC00' },
@@ -49,7 +47,6 @@ OpenSpending.Styles.Cofog = {
 	'07.4': { icon: 'icons/our-streets.svg', color: '#00CC00' },
 	'07.5': { icon: 'icons/our-streets.svg', color: '#00CC00' },
 	'07.6': { icon: 'icons/our-streets.svg', color: '#00CC00' },
-	'07.7': { icon: 'icons/our-streets.svg', color: '#00CC00' },
 
 	'08': { icon: 'icons/fire-brigade.svg', color: '#00CC00' },
 	'08.1': { icon: 'icons/fire-brigade.svg', color: '#00CC00' },
@@ -60,19 +57,21 @@ OpenSpending.Styles.Cofog = {
 	'09.3': { icon: 'icons/education.svg', color: '#00CC00' },
 	'09.4': { icon: 'icons/education.svg', color: '#00CC00' },
 	'09.5': { icon: 'icons/education.svg', color: '#00CC00' },
-	'09.6': { icon: 'icons/education.svg', color: '#00CC00' },
-	'09.7': { icon: 'icons/education.svg', color: '#00CC00' },
-	'09.8': { icon: 'icons/education.svg', color: '#00CC00' },
-	'09.9': { icon: 'icons/education.svg', color: '#00CC00' },
 
 	'10': { icon: 'icons/economic-aid.svg', color: '#00CC00' },
 	'10.1': { icon: 'icons/economic-aid.svg', color: '#00CC00' },
+	'10.2': { icon: 'icons/economic-aid.svg', color: '#00CC00' },
+	'10.3': { icon: 'icons/economic-aid.svg', color: '#00CC00' },
+	'10.4': { icon: 'icons/economic-aid.svg', color: '#00CC00' },
 
-	'11': { icon: 'icons/aid-developing-countries.svg', color: '#00CC00' },
-	'11.1': { icon: 'icons/aid-developing-countries.svg', color: '#00CC00' },
 
-	'12': { icon: 'icons/publicaffairs.svg', color: '#00CC00' },
-	'12.1': { icon: 'icons/publicaffairs.svg', color: '#00CC00' },
+	'11': { icon: 'icons/publicaffairs.svg', color: '#00CC00' },
+	'11.1': { icon: 'icons/publicaffairs.svg', color: '#00CC00' },
+
+	'12': { icon: 'icons/aid-developing-countries.svg', color: '#00CC00' },
+	'12.1': { icon: 'icons/aid-developing-countries.svg', color: '#00CC00' },
+	'12.2': { icon: 'icons/aid-developing-countries.svg', color: '#00CC00' },
+	'12.3': { icon: 'icons/aid-developing-countries.svg', color: '#00CC00' },
 
 	'13': { icon: 'icons/money.svg', color: '#00CC00' },
 	'13.1': { icon: 'icons/money.svg', color: '#00CC00' }
